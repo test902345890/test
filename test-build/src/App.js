473c1b68190314cb8778f4ca006265f1126be39e
Yurii Z <yzviahintsev@life360.com>
2023-07-14 13:49:33 -0400

    003

--- test-build/src/App.js
+++ test-build/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import Steve from './img/steve.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div style={ { borderRadius: '50%', overflow: 'hidden' }} className="App-logo" >
+          <img alt="Steve" src={Steve} />
+        </div>
         <h1>Another deploy!</h1>
       </header>
     </div>
