48426e9effcc335c76faa8e92a03f70ce3aa92a9
Yurii Z <yzviahintsev@life360.com>
2023-07-15 08:03:28 -0400

    006

--- test-build/src/App.js
+++ test-build/src/App.js
@@ -1,14 +1,26 @@
-import Steve from './img/steve.png';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 5000);
+
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div style={ { borderRadius: '50%', overflow: 'hidden' }} className="App-logo" >
-          <img alt="Steve" src={Steve} />
+        <div style={ {border: '3px inset #fff', width: '250px', height: '250px', borderRadius: '50%', overflow: 'hidden', lineHeight: '250px', textAlign: 'middle' }} className="App-logo" >
+          {time}
         </div>
-        <h1>Happy Friday!</h1>
       </header>
     </div>
   );
